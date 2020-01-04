c203c3fbafe1aee1b17ce0b07cc1c888396acd2a
Milosz Gawel <mgawel@gmail.com>
2020-01-05 00:19:40 +0100

    Updates

---
+++ precache-manifest.d1f9e3ede27b974509ca99d537716e1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12dc5a3543bcca84396868a741f21733",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "a7db1e3829f122dfa538",
+    "url": "/currency-converter/static/css/main.1ad93cbd.chunk.css"
+  },
+  {
+    "revision": "5734de6f44b15c6ced15",
+    "url": "/currency-converter/static/js/2.b4d076ea.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/currency-converter/static/js/2.b4d076ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7db1e3829f122dfa538",
+    "url": "/currency-converter/static/js/main.197d6c44.chunk.js"
+  },
+  {
+    "revision": "e470948f0f9c3af28046",
+    "url": "/currency-converter/static/js/runtime-main.2425c0c0.js"
+  },
+  {
+    "revision": "5621c2a3e864009983b118f4d48d6492",
+    "url": "/currency-converter/static/media/logo.5621c2a3.png"
+  },
+  {
+    "revision": "0e18f56496203aea40fa071c6bcb5ab6",
+    "url": "/currency-converter/static/media/money.0e18f564.jpeg"
+  }
+]);
